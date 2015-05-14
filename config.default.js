f266861111bd13d05e02ca3ff1e71f0e145d376d
Alistair McKinlay <github@10people.co.uk>
2015-05-14 15:27:07 +0100

    Add config.default.js

---
+++ config.default.js
@@ -0,0 +1,14 @@
+export {
+  "server": "irc.synirc.net",
+  "port": "6669",
+  "nick": "porygon-bot",
+  "userName": "porygon-bot",
+  "realName": "Porygon Bot",
+  "password": "super-secret-password",
+  "dbHost": "http://localhost",
+  "dbUser": "mysql",
+  "dbPassword": "super-secret-password",
+  "database": "porygon-bot",
+  "channels": ["#ircroom"],
+  "friendly": []
+}
