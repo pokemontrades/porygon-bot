840a7c1ba5e9f42f53ab72cf750c36aec4632ba9
not-an-aardvark <notaardvark@gmail.com>
2015-12-30 15:57:12 -0500

    Added other search shortcuts

--- commands/modmail.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  message_regex: /^.modmail(?: (.+)|$)/,
-  response: function (message_match) {
-    return 'http://hq.porygon.co/search/modmail/' + encodeURIComponent(message_match[1] || '').replace(/[!'\(\)]/g, escape);
-  }
-};
