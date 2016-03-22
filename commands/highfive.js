51180647caf461ef32d1e1cee9b1a9b1b10090e8
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-03-21 23:33:10 -0500

    'Fix' high-fives
    Apparently other peoples' high-fives aren't as dramatic as mine

--- commands/highfive.js
+++ commands/highfive.js
@@ -7,10 +7,10 @@ module.exports = {
       return;
     }
     if (stored.length && stored.slice(-1)[0].direction === 'left' && message_match[1] === 'o/') {
-      return stored.pop().author + ' o/\\o ' + author_match[0];
+      return author_match[0] + ' o/\\o ' + stored.pop().author;
     }
     if (stored.length && stored.slice(-1)[0].direction === 'right' && message_match[1] === '\\o') {
-      return author_match[0] + ' o/\\o ' + stored.pop().author;
+      return stored.pop().author + ' o/\\o ' + author_match[0];
     }
     stored.push({author: author_match[0], direction: message_match[1] === 'o/' ? 'right' : 'left'});
     if (!STACKABLE && stored.length > 1) {
