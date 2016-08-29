b039aa333bfbecb3393db4dc290d13bdead1d647
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-08-28 20:36:17 -0400

    Fix an issue where isPM didn't get passed to response functions

--- bot.js
+++ bot.js
@@ -98,7 +98,7 @@ function executeCommands (event, author, channel, text) {
         if (message_match && author_match && author !== bot.nick && (isPM || checkEnabled(channel, i, config.channels[channel]))) {
             Promise.join(checkIfUserIsMod(author), checkAuthenticated(author), (isMod, isAuthenticated) => {
                 if ((commands[event][i].allow || defaultAllow)({isPM, isMod, isAuthenticated})) {
-                    outputResponse(target, commands[event][i].response({bot, message_match, author_match, channel, isMod, isAuthenticated, eventType: event}));
+                    outputResponse(target, commands[event][i].response({bot, message_match, author_match, channel, isMod, isAuthenticated, eventType: event, isPM}));
                 }
             }).catch(_.partial(handleError, target));
         }
