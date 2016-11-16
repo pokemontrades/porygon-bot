6971e08a8b303320d3d26393ac09c602f0b8ff8f
Raia <Raia@users.noreply.github.com>
2016-11-15 16:47:52 -0800

    Disallow use of search command through PM

--- commands/search.js
+++ commands/search.js
@@ -1,5 +1,8 @@
 module.exports = {
   message_regex: /^\.(ref|user|log|modmail)(?: (.+)|$)/,
+  allow: function ({isPM, isAuthenticated, isMod}) {
+    return !isPM && isAuthenticated && isMod;
+  },
   response: function ({message_match}) {
     return 'http://hq.porygon.co/search/' + message_match[1] + '/' + encodeURIComponent(message_match[2] || '').replace(/[!'\(\)]/g, escape);
   }
