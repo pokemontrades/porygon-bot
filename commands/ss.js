f55fa9dd51d63bdef92518477fd0c1f6f10b6fa4
SnowPhoenix <SnowPhoenix@users.noreply.github.com>
2017-02-13 01:18:24 -0500

    Updated .ss command to handle a third case.

--- commands/ss.js
+++ commands/ss.js
@@ -1,23 +1,24 @@
 'use strict';
 const rp = require('request-promise');
 
+function findMatch(html) {
+  let res = html.match(/var spreadsheetId = "([\w\-]+)"/);
+  if (res) {
+    return `https://docs.google.com/spreadsheets/d/${res[1]}/pubhtml`;
+  }
+}
+
 module.exports = {
   message_regex: /^\.ss (https:\/\/[^\?#]+\/\?([\w\-]{6,})[^#]*|https?:\/\/[^# ]*)(#.*)?/,
   response: function ({message_match}) {
     if (message_match[2] !== undefined) {
       return `https://docs.google.com/spreadsheets/d/${message_match[2]}/pubhtml`;
     } else {
-      let requestUrl = message_match[1].slice(-1) === '/' ? message_match[1] : message_match[1] + '/';
-      return rp(requestUrl+'static/config.js').then(function (body) {
-        let id = body.match(/var spreadsheetId = "([\w\-]+)"/)
-        if (id) {
-          return `https://docs.google.com/spreadsheets/d/${id[1]}/pubhtml`;
-        } else {
-          return 'Unable to parse spreadsheet URL';
-        }
-      }).catch(function (err) {
-        return 'Unable to find spreadsheet configuration.';
-      });
+      return rp(message_match[1]).then(
+        body => findMatch(body) || rp({baseUrl: message_match[1], url: 'static/config.js'}).then(
+          body => findMatch(body) || 'Unable to find spreadsheet configuration.'
+        ).catch(() => 'Unable to retrieve spreadsheet configuration.')
+      ).catch(() => 'Error fetching the specified URL.');
     }
   }
 };
